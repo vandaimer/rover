8643cae902ed6d7b81ab279549719d9dee2d8b86
Luiz Filipe <moresco.luiz@gmail.com>
2020-11-02 21:30:22 -0300

    First version of CLI

--- main.py
+++ main.py
@@ -1,23 +1,44 @@
-from robot import Robot
+from rover import Rover
+
 
 def main():
-    MAX_Y = 5
-    MAX_X = 5
+    print('Enter the grid size. Example: 5 5')
+    grid = input().split(' ')
+
+    print('Enter the initial position of the first rover. Example: 1 2 N')
+    first_rover_input = input().split(' ')
+    print(first_rover_input)
+    print('Enter the commands for the first rover. Example: LMLMLMLMM')
+    commands_first_rover = input()
+    print(commands_first_rover)
+
+    print('Enter the initial position of the second rover. Example: 3 3 E')
+    second_rover_input = input().split(' ')
+    print(second_rover_input)
+    print('Enter the commands for the second rover. Example: MMRMMRMRRM')
+    commands_second_rover = input()
+    print(commands_second_rover)
+
+    MAX_Y = grid[0]
+    MAX_X = grid[1]
     commands = []
 
-    # r = Robot(1, 2, 'N')
-    # commands[:] = 'LMLMLMLMM'
-    # print('--Starting--', r.current_position())
-    # for command in commands:
-    #     r.handle_command(command, MAX_X, MAX_Y)
-    #     print(r.current_position())
+    first_rover = Rover(first_rover_input[0], first_rover_input[1], first_rover_input[2])
+    second_rover = Rover(second_rover_input[0], second_rover_input[1], second_rover_input[2])
 
-    r = Robot(3, 3, 'E')
-    commands[:] = 'MMRMMRMRRM'
-    print('--Starting--', r.current_position())
+    commands[:] = commands_first_rover
+
     for command in commands:
-        r.handle_command(command, MAX_X, MAX_Y)
-        print(r.current_position())
+        first_rover.handle_command(command, MAX_X, MAX_Y)
+
+    commands[:] = commands_second_rover
+
+    for command in commands:
+        second_rover.handle_command(command, MAX_X, MAX_Y)
+
+    print(first_rover.current_position())
+    print(second_rover.current_position())
+
 
 
 if __name__ == "__main__":
